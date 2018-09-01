90501c8f05d62e92d391d4c424b209037c6f6c98
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-09-01 15:08:32 +0300

    code improved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { CarouselComponent } from './carousel/carousel.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { DataService } from './data.service';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
-    CarouselComponent,
-    FooterComponent
-  ],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
