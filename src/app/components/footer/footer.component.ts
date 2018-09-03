93f2a447dfc5fc72f43ca7661264770530ef8c78
uladzimir-yeudakimovich <uladzimir.yeudakimovich@gmail.com>
2018-09-03 12:57:25 +0300

    added form validation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -29,11 +29,27 @@ export class FooterComponent implements OnInit {
     });
   }
 
-  addMessage() {
-    this.model = [];
-    this.messages.forEach((n) => this.model.push(n));
-    this.model.push(this.later);
-    this.messageService.updateMessage({mess: this.model});
+  setMessage() {
+    if (this.later.name === '') {
+      document.getElementById('laterName').style.border = 'solid red';
+    } else {document.getElementById('laterName').style.border = '';}
+    if (this.later.email === '') {
+      document.getElementById('laterEmail').style.border = 'solid red';
+    } else {document.getElementById('laterEmail').style.border = '';}
+    if (this.later.message === '') {
+      document.getElementById('laterMessage').style.border = 'solid red';
+    } else {document.getElementById('laterMessage').style.border = '';}
+    if (this.later.name !== '' && this.later.email !== '' && this.later.message !== '') {
+      this.model = [];
+      this.messages.forEach((n) => this.model.push(n));
+      this.model.push(this.later);
+      this.messageService.updateMessage({mess: this.model});
+      this.later = {
+        name: '',
+        email: '',
+        message: ''
+      }
+    }
   }
 
 }
